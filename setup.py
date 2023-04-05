f8125d906b22a33b11a5889a9281ad7f56d36a6e
redruin1 <tfschaefer222@gmail.com>
2023-04-05 18:25:45 -0400

    2.0 is going to be one hell of an update
    like bruh

--- setup.py
+++ setup.py
@@ -58,8 +58,7 @@ setup(
     include_package_data = True,
     install_requires = [
         "schema >= 0.7.5",
-        "lupa >= 1.14.1",
-        #"lupa @ git+https://github.com/scoder/lupa.git#egg=lupa",
+        "lupa >= 2.0",
         "six >= 1.16.0",
         "typing",
         "typing_extensions; python_version < '3.8'",
